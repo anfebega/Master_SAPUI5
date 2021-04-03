35af0d519f54027bcf3f4c78c5a9eda3c7ce8536
anfebega <anfebega88@gmail.com>
2021-04-03 01:41:22 +0000

    Commit yeah!

--- Invoices/SAPUI5/webapp/controller/Details.controller.js
+++ Invoices/SAPUI5/webapp/controller/Details.controller.js
@@ -16,7 +16,8 @@ sap.ui.define([
         return Controller.extend("logaligroup.SAPUI5.controller.Details", {
 
             _onObjectMatch: function (oEvent){
-
+                //Resetea el rating
+                this.byId("rating").reset();
                 this.getView().bindElement({
                     path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                     model: "northwind"
@@ -40,6 +41,14 @@ sap.ui.define([
                     const oRouter = UIComponent.getRouterFor(this);
                     oRouter.navTo("RouteApp", {}, true);
                 }
+            },
+
+            onRatingChange: function (oEvent) {
+
+                const fValue = oEvent.getParameter("value");
+                const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+                sap.m.MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
             }
 
         });
